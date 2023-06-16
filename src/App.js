00f903d48b7d9d8be964c4e7e676de11f7b419f0
Melinda Mott <melinda.mott@gmail.com>
2023-06-16 14:31:15 -0700

    Called it in the dive element toggletask

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ const App = () => {
       });
     });
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div><TaskList tasks={TASKS} toggleTaskCompletion={toggleTaskCompletion}/></div>
       </main>
     </div>
   );
