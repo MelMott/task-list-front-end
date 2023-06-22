f5efcbd3a05945e531f2d2398a3b34ab1be5cfbf
Melinda Mott <melinda.mott@gmail.com>
2023-06-21 22:55:06 -0700

    Setting to complete and incomplete updates database and onclick

--- src/App.js
+++ src/App.js
@@ -3,51 +3,23 @@ import axios from 'axios';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 
 const App = () => {
-  const [tasks, setTasks] = useState(TASKS); // Store tasks in state
+  const [tasks, setTasks] = useState([]); // Store tasks in state
   
-//   const setComplete = async (taskId) => {
-//   try {
-//     const taskToUpdate = tasks.find((task) => task.id === taskId);
-//     const updatedTask = { ...taskToUpdate, isComplete: !taskToUpdate.isComplete };
-//     const response = await axios.patch(`https://task-list-api-c17.onrender.com/tasks/${taskId}/${updatedTask.isComplete ? 'mark_complete' : 'mark_incomplete'}`, updatedTask);
-//     setTasks((prevTasks) => prevTasks.map((task) => (task.id === taskId ? response.data : task)));
-//   } catch (error) {
-//     console.error('Error updating task:', error);
-//   }
-// };
-
-  const setComplete = (taskId) => {
-    setTasks((prevTasks) => {
-      return prevTasks.map((task) => {
-        if (task.id === taskId) {
-          return { ...task, 'isComplete': !task.isComplete };
-        }
-        return task;
-      });
-    });
-  };
-
-  const removeTask = (taskId) => {
-    setTasks((prevTasks) => {
-      return prevTasks.filter((task) => task.id !== taskId);
-    });
-  };
-
   useEffect(() => {
     const fetchTasks = async () => {
       try {
@@ -60,6 +32,36 @@ const App = () => {
   
     fetchTasks();
   }, []);
+  
+  const setComplete = async (taskId) => {
+  try {
+    const taskToUpdate = tasks.find((task) => task.id === taskId);
+    const updatedTask = { ...taskToUpdate, isComplete: !taskToUpdate.isComplete };
+    const endpoint = updatedTask.isComplete ? 'mark_complete' : 'mark_incomplete';
+    const response = await axios.patch(`http://localhost:5000/tasks/${taskId}/${endpoint}`, updatedTask);
+    setTasks((prevTasks) => prevTasks.map((task) => (task.id === taskId ? { ...task, isComplete: updatedTask.isComplete } : task))
+    );
+  } catch (error) {
+    console.error('Error updating task:', error);
+  }
+};
+
+  // const setComplete = (taskId) => {
+  //   setTasks((prevTasks) => {
+  //     return prevTasks.map((task) => {
+  //       if (task.id === taskId) {
+  //         return { ...task, 'isComplete': !task.isComplete };
+  //       }
+  //       return task;
+  //     });
+  //   });
+  // };
+
+  const removeTask = (taskId) => {
+    setTasks((prevTasks) => {
+      return prevTasks.filter((task) => task.id !== taskId);
+    });
+  };
   
   return (
     <div className="App">
